c5755a0d9a567bc244af360f7fafd9be847ef42a
Aakash457 <114485477+Aakash457@users.noreply.github.com>
2026-02-24 12:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+
+btn.addEventListener("click", function () {
+  const select = document.getElementById("colorSelect");
+
+  if (select.selectedIndex !== -1) {
+    select.remove(select.selectedIndex);
+  }
+});
